e3c4a0c44262c1cc02cabe852a630f6408653891
Miguel Angel <angelarkangel91@hotmail.com>
2021-03-12 15:30:19 -0600

    Updates

---
+++ precache-manifest.69883e8248cf5f6858bfead6543948c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc8c8d083d44e1fbfc1ea3436ca81ce",
+    "url": "/tickets-app/index.html"
+  },
+  {
+    "revision": "02d1cdaa8ad238c0162b",
+    "url": "/tickets-app/static/css/main.cbe67141.chunk.css"
+  },
+  {
+    "revision": "e681ce523e9e9ac71b35",
+    "url": "/tickets-app/static/js/2.ffe2aeb2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tickets-app/static/js/2.ffe2aeb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d1cdaa8ad238c0162b",
+    "url": "/tickets-app/static/js/main.b9e83518.chunk.js"
+  },
+  {
+    "revision": "cf42ef5821c324773d15",
+    "url": "/tickets-app/static/js/runtime-main.ac88c2a2.js"
+  },
+  {
+    "revision": "1cb1d4e8e09aeb7cc631ffb3a9597354",
+    "url": "/tickets-app/static/media/cine.1cb1d4e8.png"
+  }
+]);
